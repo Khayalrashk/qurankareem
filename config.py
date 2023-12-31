fad227b5a10967949e6a6069aea262a6354d086e
abdalaleQ <153198040+abdalaleQ@users.noreply.github.com>
2023-12-31 17:44:47 +0300

    Update config.py

--- config.py
+++ config.py
@@ -13,15 +13,16 @@ DURATION_LIMIT = int(getenv("DURATION_LIMIT", "9000"))
 
 OWNER_ID = int(getenv("OWNER_ID"))
 
-PING_IMG = getenv("PING_IMG", "https://graph.org/file/4921b5d12ac8f7013ac73.jpg")
-START_IMG = getenv("START_IMG", "https://graph.org/file/4921b5d12ac8f7013ac73.jpg")
+PING_IMG = getenv("PING_IMG", "https://graph.org/file/325aa1aa99e917235149e.jpg")
+START_IMG = getenv("START_IMG", "https://graph.org/file/325aa1aa99e917235149e.jpg")
 
 SESSION = getenv("SESSION", None)
 
-SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/D_FBJ")
-SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/pp_g3")
+SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/rr_r_v")
+SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/rr_r_v")
 
 SUDO_USERS = list(map(int, getenv("SUDO_USERS", "5190136458").split()))
 
 
-FAILED = "https://graph.org/file/4921b5d12ac8f7013ac73.jpg"
+FAILED = "
+https://graph.org/file/325aa1aa99e917235149e.jpg"
